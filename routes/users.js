fd5cd2a6f6c210fcd8bdb01c76ed92e49016227a
AlexisNichel <alexisgranja@gmail.com>
2015-12-10 15:12:28 +0000

    registra nuevo usuario en db

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+var login = new sqlite3.Database('./database/Usuarios.sqlite');
 router.get('/', function(req, res) {
 	var LocalStorage = require('node-localstorage').LocalStorage;
 var admin = localStorage.getItem('admin');
@@ -9,5 +11,16 @@ res.render(__dirname + '/../views/registrausu.jade');
 	res.redirect('/');
 };
 	});
+
+router.post('/', function (req, res) {
+			var usuario=req.body.username;
+			var password=req.body.password;
+			var tipo="";
+			login.serialize(function () {
+			var insertar = login.prepare("INSERT INTO users VALUES (?,?,?)");
+			insertar.run(usuario, password, tipo);
+			insertar.finalize();
+			});
+});
 
 module.exports = router;
